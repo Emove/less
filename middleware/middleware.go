61a17e4d435346415004119a41e94e47037e12e4
Emove@668466 <1019291811@qq.com>
2022-09-11 18:58:42 +0800

    add middleware

---
+++ middleware/middleware.go
@@ -0,0 +1,18 @@
+package middleware
+
+import (
+	"context"
+)
+
+type Handler func(ctx context.Context, message interface{}) error
+
+type Middleware func(handler Handler) Handler
+
+func Chain(ms ...Middleware) Middleware {
+	return func(next Handler) Handler {
+		for i := len(ms) - 1; i >= 0; i-- {
+			next = ms[i](next)
+		}
+		return next
+	}
+}
---
+++ middleware/middleware_test.go
@@ -0,0 +1,58 @@
+package middleware
+
+import (
+	"context"
+	"testing"
+)
+
+func TestChain(t *testing.T) {
+
+	h := func(ctx context.Context, message interface{}) error {
+		t.Logf("handle message: %v", message)
+		return nil
+	}
+
+	err := Chain(middleware1(t), middleware2(t), middleware3(t))(h)(context.Background(), "middleware test")
+	if err != nil {
+		t.Errorf("expect %v, got %v", nil, err)
+	}
+}
+
+func middleware1(t *testing.T) Middleware {
+	return func(handler Handler) Handler {
+		return func(ctx context.Context, message interface{}) error {
+			t.Logf("middleware1 before...")
+
+			err := handler(ctx, message)
+
+			t.Logf("middleware1 after...")
+			return err
+		}
+	}
+}
+
+func middleware2(t *testing.T) Middleware {
+	return func(handler Handler) Handler {
+		return func(ctx context.Context, message interface{}) error {
+			t.Logf("middleware2 before...")
+
+			err := handler(ctx, message)
+
+			t.Logf("middleware2 after...")
+			return err
+		}
+	}
+}
+
+func middleware3(t *testing.T) Middleware {
+	return func(handler Handler) Handler {
+		return func(ctx context.Context, message interface{}) error {
+			t.Logf("middleware3 before...")
+
+			err := handler(ctx, message)
+
+			t.Logf("middleware3 after...")
+			return err
+		}
+	}
+}
