0b1866ba06772bd3f1e573f7d79f93a3bfd2ae97
Emove@668466 <1019291811@qq.com>
2022-09-11 20:25:01 +0800

    refactor codec

--- codec/codec.go
+++ codec/codec.go
@@ -1,7 +1,15 @@
 package codec
 
-type Codec interface {
+import "github.com/emove/less/io"
+
+type PacketCodec interface {
 	Name() string
-	Marshal(msg interface{}) ([]byte, error)
-	UnMarshal(data []byte, msg interface{}) error
+	Encode(message interface{}, writer io.Writer, payloadCodec PayloadCodec) (err error)
+	Decode(reader io.Reader, payloadCodec PayloadCodec) (message interface{}, err error)
+}
+
+type PayloadCodec interface {
+	Name() string
+	Marshal(msg interface{}, writer io.Writer) (err error)
+	UnMarshal(reader io.Reader) (msg interface{}, err error)
 }
