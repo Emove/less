1de455f3eabfc86a7902b8e42730ea973e0a9c92
Emove@668466 <1019291811@qq.com>
2022-09-14 23:58:32 +0800

    [feat] implements io.Reader and add Length method

--- pkg/io/reader/limit.go
+++ pkg/io/reader/limit.go
@@ -2,19 +2,35 @@ package reader
 
 import (
 	"github.com/emove/less/internal/errors"
-	io2 "github.com/emove/less/pkg/io"
+	less_io "github.com/emove/less/pkg/io"
 )
+
+// NewLimitReader returns a Reader that readable bytes limited
+func NewLimitReader(decorator less_io.Reader, limit uint32) less_io.Reader {
+	return &limitReader{
+		decorator: decorator,
+		total:     int(limit),
+		remain:    limit,
+	}
+}
 
 type limitReader struct {
 	remain    uint32
-	decorator io2.Reader
+	total     int
+	decorator less_io.Reader
 }
 
-func NewLimitReader(decorator io2.Reader, limit uint32) io2.Reader {
-	return &limitReader{
-		decorator: decorator,
-		remain:    limit,
+var _ less_io.Reader = (*limitReader)(nil)
+
+func (lr *limitReader) Read(buff []byte) (n int, err error) {
+	if int(lr.remain)-len(buff) < 0 {
+		return 0, errors.New("buffer remain not enough, remain: %d, want: %d", lr.remain, len(buff))
 	}
+	n, err = lr.decorator.Read(buff)
+	if err == nil {
+		lr.remain -= uint32(n)
+	}
+	return
 }
 
 func (lr *limitReader) Next(n int) (buf []byte, err error) {
@@ -30,7 +46,7 @@ func (lr *limitReader) Next(n int) (buf []byte, err error) {
 
 func (lr *limitReader) Peek(n int) (buf []byte, err error) {
 	if int(lr.remain)-n < 0 {
-		return buf, errors.New("buf remain not enough, remain: %d, want: %d", lr.remain, n)
+		return buf, errors.New("buffer remain not enough, remain: %d, want: %d", lr.remain, n)
 	}
 	return lr.decorator.Peek(n)
 }
@@ -44,6 +60,11 @@ func (lr *limitReader) Skip(n int) (err error) {
 	}
 	lr.remain -= uint32(n)
 	return
+}
+
+// Length returns total readable size
+func (lr *limitReader) Length() int {
+	return lr.total
 }
 
 func (lr *limitReader) Release() {
--- pkg/io/reader/limit_test.go
+++ pkg/io/reader/limit_test.go
@@ -1,6 +1,7 @@
 package reader
 
 import (
+	"bytes"
 	"testing"
 )
 
@@ -85,4 +86,70 @@ func Test_limitReader_Skip(t *testing.T) {
 			t.Fatalf("read more bytes: %s", string(next))
 		}
 	})
+}
+
+func Test_limitReader_Length(t *testing.T) {
+	buff := &bytes.Buffer{}
+	buff.WriteString("hello world")
+
+	reader := NewLimitReader(NewBufferReader(buff), 11)
+	if reader.Length() != 11 {
+		t.Fatalf("want: %d, got: %d", 11, reader.Length())
+	}
+}
+
+func Test_limitReader_Read(t *testing.T) {
+	buff := &bytes.Buffer{}
+	buff.WriteString("hello world")
+
+	reader := NewLimitReader(NewBufferReader(buff), 11)
+
+	h := make([]byte, 5, 5)
+	read, err := reader.Read(h)
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+	if read != 5 {
+		t.Fatalf("want n: %d, got n: %d", 5, read)
+	}
+
+	if string(h) != "hello" {
+		t.Fatalf("want buff: %s, got buff: %s", "hello", string(h))
+	}
+
+	// readIndex < writerIndex, but enough
+	_, _ = reader.Peek(1)
+	b := make([]byte, 1, 1)
+	read, err = reader.Read(b)
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+	if read != 1 {
+		t.Fatalf("want n: %d, got n: %d", 1, read)
+	}
+
+	if string(b) != " " {
+		t.Fatalf("want buff: %s, got buff: %s", " ", string(b))
+	}
+
+	// readIndex < writeIndex, but inside buff not enough
+	_, _ = reader.Peek(3)
+	s := make([]byte, 5, 5)
+	read, err = reader.Read(s)
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+	if read != 5 {
+		t.Fatalf("want n: %d, got n: %d", 5, read)
+	}
+
+	if string(s) != "world" {
+		t.Fatalf("want buff: %s, got buff: %s", "world", string(s))
+	}
+
+	b = make([]byte, 1, 1)
+	read, err = reader.Read(b)
+	if err != nil {
+		t.Logf("got: %v", err)
+	}
 }
